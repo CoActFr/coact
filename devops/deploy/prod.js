e82465d1ec05ee566e7ff1df47c46378768c2d35
AubryH <aubryh@theodo.fr>
2015-10-06 17:22:10 +0200

    just restart forever

--- devops/deploy/prod.js
+++ devops/deploy/prod.js
@@ -5,7 +5,7 @@ module.exports = function (shipit) {
           "cd " + shipit.releasePath +
           " && npm config set production" +
           " && npm install --unsafe-perm" +
-          " && cd www/ && COACT_PORT=" + shipit.config.serverPort + " forever start --uid '" + shipit.config.foreverUID + "' -a -o out.log -e err.log  server.js"
+          " && cd www/ && forever restart " + shipit.config.foreverUID
         );
     });
 
